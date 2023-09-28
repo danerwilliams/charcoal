b80245a00441c0727db0a649b71fccfee7f8cf83
Dane Williams <dane_williams@outlook.com>
2023-09-28 19:55:28 -0400

    attempting tree

--- apps/cli/src/actions/create_pr_body_footer.ts
+++ apps/cli/src/actions/create_pr_body_footer.ts
@@ -2,9 +2,77 @@ import { TContext } from '../lib/context';
 
 export function createPrBodyFooter(context: TContext, branch: string): string {
   const terminalParent = findTerminalParent(context, branch);
+
+  const tree = buildBranchTree({
+    context,
+    currentBranches: [terminalParent],
+    currentTree: '',
+    prBranch: branch,
+    currentDepth: 0,
+  });
+
   // eslint-disable-next-line no-console
-  console.log({ terminalParent });
+  console.log(tree);
   return '';
+}
+
+function buildBranchTree({
+  context,
+  currentBranches,
+  currentTree,
+  prBranch,
+  currentDepth,
+}: {
+  context: TContext;
+  currentBranches: string[];
+  currentTree: string;
+  prBranch: string;
+  currentDepth: number;
+}): string {
+  for (const branch of currentBranches) {
+    currentTree += `\n${buildLeaf({
+      context,
+      branch,
+      depth: currentDepth,
+      prBranch,
+    })}`;
+
+    const children = context.engine.getChildren(branch);
+
+    if (children.length) {
+      currentTree += `\n${buildBranchTree({
+        context,
+        currentBranches: children,
+        currentTree,
+        prBranch,
+        currentDepth: currentDepth + 1,
+      })}`;
+    }
+  }
+
+  return currentTree;
+}
+
+function buildLeaf({
+  context,
+  branch,
+  depth,
+  prBranch,
+}: {
+  context: TContext;
+  branch: string;
+  depth: number;
+  prBranch: string;
+}): string {
+  const prInfo = context.engine.getPrInfo(branch);
+
+  if (!prInfo?.number) {
+    throw new Error('PR number is undefined');
+  }
+
+  return `${' '.repeat(depth)}* **PR #${prInfo.number}**${
+    branch === prBranch ? ' 👈' : ''
+  }`;
 }
 
 function findTerminalParent(context: TContext, currentBranch: string): string {
