dc1779c60358a9ba98ae541bfd60d35d204e5548
Dane Williams <dane_williams@outlook.com>
2024-02-08 22:18:02 -0500

    seems to work

--- apps/cli/test/actions/submit_action.test.ts
+++ apps/cli/test/actions/submit_action.test.ts
@@ -1,125 +1,125 @@
 import { expect, use } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-// import { inferPRBody } from '../../src/actions/submit/pr_body';
-// import { getPRTitle } from '../../src/actions/submit/pr_title';
+import { inferPRBody } from '../../src/actions/submit/pr_body';
+import { getPRTitle } from '../../src/actions/submit/pr_title';
 import { updatePrBodyFooter } from '../../src/actions/submit/submit_action';
-// import { validateNoEmptyBranches } from '../../src/actions/submit/validate_branches';
-// import { BasicScene } from '../lib/scenes/basic_scene';
-// import { configureTest } from '../lib/utils/configure_test';
+import { validateNoEmptyBranches } from '../../src/actions/submit/validate_branches';
+import { BasicScene } from '../lib/scenes/basic_scene';
+import { configureTest } from '../lib/utils/configure_test';
 
 use(chaiAsPromised);
 
-// for (const scene of [new BasicScene()]) {
-//   describe(`(${scene}): correctly infers submit info from commits`, function () {
-//     configureTest(this, scene);
+for (const scene of [new BasicScene()]) {
+  describe(`(${scene}): correctly infers submit info from commits`, function () {
+    configureTest(this, scene);
 
-//     it('can infer title/body from single commit', async () => {
-//       const title = 'Test Title';
-//       const body = ['Test body line 1.', 'Test body line 2.'].join('\n');
-//       const message = `${title}\n\n${body}`;
+    it('can infer title/body from single commit', async () => {
+      const title = 'Test Title';
+      const body = ['Test body line 1.', 'Test body line 2.'].join('\n');
+      const message = `${title}\n\n${body}`;
 
-//       scene.repo.createChange('a');
-//       scene.repo.runCliCommand([`branch`, `create`, `a`, `-m`, message]);
+      scene.repo.createChange('a');
+      scene.repo.runCliCommand([`branch`, `create`, `a`, `-m`, message]);
 
-//       expect(
-//         await getPRTitle(
-//           { branchName: 'a', editPRFieldsInline: false },
-//           scene.getContext()
-//         )
-//       ).to.equals(title);
-//       expect(
-//         inferPRBody(
-//           { branchName: 'a', template: 'template' },
-//           scene.getContext()
-//         ).inferredBody
-//       ).to.equals(`template`);
+      expect(
+        await getPRTitle(
+          { branchName: 'a', editPRFieldsInline: false },
+          scene.getContext()
+        )
+      ).to.equals(title);
+      expect(
+        inferPRBody(
+          { branchName: 'a', template: 'template' },
+          scene.getContext()
+        ).inferredBody
+      ).to.equals(`template`);
 
-//       scene
-//         .getContext()
-//         .userConfig.update((data) => (data.submitIncludeCommitMessages = true));
+      scene
+        .getContext()
+        .userConfig.update((data) => (data.submitIncludeCommitMessages = true));
 
-//       expect(
-//         await getPRTitle(
-//           { branchName: 'a', editPRFieldsInline: false },
-//           scene.getContext()
-//         )
-//       ).to.equals(title);
-//       expect(
-//         inferPRBody(
-//           { branchName: 'a', template: 'template' },
-//           scene.getContext()
-//         ).inferredBody
-//       ).to.equals(`${body}\n\ntemplate`);
-//     });
+      expect(
+        await getPRTitle(
+          { branchName: 'a', editPRFieldsInline: false },
+          scene.getContext()
+        )
+      ).to.equals(title);
+      expect(
+        inferPRBody(
+          { branchName: 'a', template: 'template' },
+          scene.getContext()
+        ).inferredBody
+      ).to.equals(`${body}\n\ntemplate`);
+    });
 
-//     it('can infer just title with no body', async () => {
-//       const title = 'Test Title';
-//       const commitMessage = title;
+    it('can infer just title with no body', async () => {
+      const title = 'Test Title';
+      const commitMessage = title;
 
-//       scene.repo.createChange('a');
-//       scene.repo.runCliCommand([`branch`, `create`, `a`, `-m`, commitMessage]);
+      scene.repo.createChange('a');
+      scene.repo.runCliCommand([`branch`, `create`, `a`, `-m`, commitMessage]);
 
-//       expect(
-//         await getPRTitle(
-//           { branchName: 'a', editPRFieldsInline: false },
-//           scene.getContext()
-//         )
-//       ).to.equals(title);
-//       expect(
-//         inferPRBody(
-//           { branchName: 'a', template: 'template' },
-//           scene.getContext()
-//         ).inferredBody
-//       ).to.equal('template');
-//     });
+      expect(
+        await getPRTitle(
+          { branchName: 'a', editPRFieldsInline: false },
+          scene.getContext()
+        )
+      ).to.equals(title);
+      expect(
+        inferPRBody(
+          { branchName: 'a', template: 'template' },
+          scene.getContext()
+        ).inferredBody
+      ).to.equal('template');
+    });
 
-//     it('can infer title/body from multiple commits', async () => {
-//       const title = 'Test Title';
-//       const secondSubj = 'Second commit subject';
+    it('can infer title/body from multiple commits', async () => {
+      const title = 'Test Title';
+      const secondSubj = 'Second commit subject';
 
-//       scene.repo.createChange('a');
-//       scene.repo.runCliCommand([`branch`, `create`, `a`, `-m`, title]);
-//       scene.repo.createChangeAndCommit(secondSubj);
+      scene.repo.createChange('a');
+      scene.repo.runCliCommand([`branch`, `create`, `a`, `-m`, title]);
+      scene.repo.createChangeAndCommit(secondSubj);
 
-//       expect(
-//         await getPRTitle(
-//           { branchName: 'a', editPRFieldsInline: false },
-//           scene.getContext()
-//         )
-//       ).to.equals(title);
-//       expect(
-//         inferPRBody({ branchName: 'a' }, scene.getContext()).inferredBody
-//       ).to.equal(``);
+      expect(
+        await getPRTitle(
+          { branchName: 'a', editPRFieldsInline: false },
+          scene.getContext()
+        )
+      ).to.equals(title);
+      expect(
+        inferPRBody({ branchName: 'a' }, scene.getContext()).inferredBody
+      ).to.equal(``);
 
-//       scene
-//         .getContext()
-//         .userConfig.update((data) => (data.submitIncludeCommitMessages = true));
+      scene
+        .getContext()
+        .userConfig.update((data) => (data.submitIncludeCommitMessages = true));
 
-//       expect(
-//         await getPRTitle(
-//           { branchName: 'a', editPRFieldsInline: false },
-//           scene.getContext()
-//         )
-//       ).to.equals(title);
-//       expect(
-//         inferPRBody({ branchName: 'a' }, scene.getContext()).inferredBody
-//       ).to.equal(`${title}\n\n${secondSubj}`);
-//     });
+      expect(
+        await getPRTitle(
+          { branchName: 'a', editPRFieldsInline: false },
+          scene.getContext()
+        )
+      ).to.equals(title);
+      expect(
+        inferPRBody({ branchName: 'a' }, scene.getContext()).inferredBody
+      ).to.equal(`${title}\n\n${secondSubj}`);
+    });
 
-//     it('aborts if the branch is empty', async () => {
-//       scene.repo.runCliCommand([`branch`, `create`, `a`, `-m`, `a`]);
-//       await expect(validateNoEmptyBranches(['a'], scene.getContext())).to.be
-//         .rejected;
-//     });
+    it('aborts if the branch is empty', async () => {
+      scene.repo.runCliCommand([`branch`, `create`, `a`, `-m`, `a`]);
+      await expect(validateNoEmptyBranches(['a'], scene.getContext())).to.be
+        .rejected;
+    });
 
-//     it('does not abort if the branch is not empty', async () => {
-//       scene.repo.createChange('a');
-//       scene.repo.runCliCommand([`branch`, `create`, `a`, `-m`, `a`]);
-//       await expect(validateNoEmptyBranches(['a'], scene.getContext())).to.be
-//         .fulfilled;
-//     });
-//   });
-// }
+    it('does not abort if the branch is not empty', async () => {
+      scene.repo.createChange('a');
+      scene.repo.runCliCommand([`branch`, `create`, `a`, `-m`, `a`]);
+      await expect(validateNoEmptyBranches(['a'], scene.getContext())).to.be
+        .fulfilled;
+    });
+  });
+}
 
 describe('updatePrBodyFooter', () => {
   const newFooter = `
